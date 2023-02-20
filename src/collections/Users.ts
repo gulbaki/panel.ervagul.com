bfae365c2bff59256e9e0c0f45d9a7d6f4ebdf02
baki gul <bbkgull@gmail.com>
2023-02-20 20:10:18 +0300

    usersj updatd

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -18,7 +18,18 @@ const Users: CollectionConfig = {
     {
       name: 'name',
       type: 'text',
-    }
+    },
+      {
+        name: 'role',
+        type: 'select',
+        options: [
+          { label: 'Admin', value: 'admin' },
+          { label: 'User', value: 'user' },
+        ],
+        required: true,
+        defaultValue: 'user',
+      },
+  
   ],
 };
 
