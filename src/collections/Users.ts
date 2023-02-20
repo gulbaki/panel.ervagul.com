6534b5b3cf579e5c6c6b5f1e30d178ff9cca97cd
baki gul <bbkgull@gmail.com>
2023-02-20 20:24:15 +0300

    serverurl updated

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -19,16 +19,16 @@ const Users: CollectionConfig = {
       name: 'name',
       type: 'text',
     },
-      {
-        name: 'role',
-        type: 'select',
-        options: [
-          { label: 'Admin', value: 'admin' },
-          { label: 'User', value: 'user' },
-        ],
-        required: true,
-        defaultValue: 'user',
-      },
+    {
+      name: 'role',
+      type: 'select',
+      options: [
+        { label: 'Admin', value: 'admin' },
+        { label: 'User', value: 'user' },
+      ],
+      required: true,
+      defaultValue: 'user',
+    },
   
   ],
 };
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -13,7 +13,7 @@ import { Social } from './globals/Social';
 
 export default buildConfig({
   serverURL: process.env.SERVER_URL,
-  cors:['*', 'http://localhost:3001', '0.0.0.0:3001','https://ervagul.com', 'http://localhost:4006'],
+  cors:['*', 'http://localhost:3001', '0.0.0.0:3001','https://ervagul.com', 'http://localhost:4005'],
   admin: {
     user: Users.slug,
   },
